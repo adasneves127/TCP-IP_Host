b26d4305830a5cfe08b30b44a96458fdada66530
Alex Dasneves <adasneves127@gmail.com>
2021-11-15 19:35:02 -0500

    Initial Commit

---
+++ TCP-IP Host/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Runtime.ConstrainedExecution;
+
+namespace TCP_IP_Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TCP/IP Test Project Host");
+            Console.WriteLine();
+            Console.WriteLine("Current IP: " + GetLocalIPv4(NetworkInterfaceType.Wireless80211));
+
+        }
+        public static string GetLocalIPv4(NetworkInterfaceType _type)
+        {
+            string output = "";
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            output = ip.Address.ToString();
+                        }
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
